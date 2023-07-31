b3bcd9f801fac95b3c0cdfbee417ed2a8d91c0bd
asaita <saitayyildiz@gmail.com>
2023-07-31 16:28:48 +0300

    51.ders

--- app/Http/Controllers/ListingController.php
+++ app/Http/Controllers/ListingController.php
@@ -32,7 +32,18 @@ class ListingController extends Controller
      */
     public function store(Request $request)
     {
-        Listing::create($request->all());
+        Listing::create([
+            ...$request->all(),
+            ...$request->validate([
+                'beds'=>'required|integer|min:0|max:10',
+                'baths'=>'required|integer|min:0|max:10',
+                'area'=>'required|integer|min:30|max:500',
+                'city'=>'required',
+                'code'=>'required',
+                'street'=>'required',
+                'street_nr'=>'required|integer|min:1|max:1000',
+                'price'=>'required|integer|min:1|max:20000000',            ])
+        ]);
 
         return redirect()->route('listing.index')->with('success','listing was created!');
        
